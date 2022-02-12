0ce3648d104b95d46a942780227d3c7a505524a2
David Paul Graham <dpgraham4401@gmail.com>
2022-02-12 09:30:32 -0500

    add stowAll.sh script

---
+++ stowAll.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+Help()
+{
+   # Display Help
+   echo "Use GNU stow to stash all config in appropriate place"
+   echo
+   echo "Syntax: stowAll.sh [ -i | h ] "
+   echo "options:"
+   echo "i     ignore these directories"
+   echo "h     Print this help and exit"
+   echo 
+   echo "example ./stowAll.sh -i 'sway scripts' "
+   echo 
+}
+
+DOTFILE_DIR="$HOME/.dotfiles"
+
+while getopts i:h flag
+do
+    case "${flag}" in
+        i) ignore="$ignore ${OPTARG}";;
+        h) 
+            Help
+            exit;;
+    esac
+done
+
+# stow all packages
+if command -v stow &> /dev/null
+then
+    for entry in "$DOTFILE_DIR"/*
+    do
+        package="$(basename $entry)"
+        if [[ -d $package ]]
+        then
+            if [[ $ignore == *"$package"* ]]
+            then
+                echo "ignoring "$(basename $entry)
+            else
+                echo "stowing: "$(basename $entry)
+                stow $(basename $entry)
+            fi
+        fi
+    done
+else
+    echo "GNU stow command not found"
+fi
+
+
