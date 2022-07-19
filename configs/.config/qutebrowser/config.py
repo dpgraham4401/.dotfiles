3f7f809736572931ada38fbd3cf4c712a7a36876
David Graham <dpgraham4401@gmail.com>
2022-07-19 14:52:48 -0400

    qutebrowser and fedora_sway packages

--- configs/.config/qutebrowser/config.py
+++ configs/.config/qutebrowser/config.py
@@ -10,24 +10,6 @@ c.completion.height = '30%'
 c.statusbar.padding = {'bottom': 5, 'left': 5, 'right': 5, 'top': 5}
 c.statusbar.widgets = ["keypress", "url", "scroll", "history", "tabs", "progress"]
 c.statusbar.position = 'bottom'
-# c.colors.completion.fg = 'black'
-# c.colors.completion.odd.bg = '#F2F2F2'
-# c.colors.completion.even.bg = 'lightgrey'
-# c.colors.completion.category.fg = 'black'
-# c.colors.completion.category.bg = 'lightgrey'
-# c.colors.completion.item.selected.fg = 'black'
-# c.colors.completion.item.selected.bg = 'darkgrey'
-# c.colors.completion.item.selected.border.top = 'black'
-# c.colors.completion.item.selected.border.bottom = 'black'
-# c.colors.completion.match.fg = '#ff4444'
-# c.colors.completion.scrollbar.fg = 'white'
-# c.colors.completion.scrollbar.bg = 'darkgrey'
-# c.colors.statusbar.url.error.fg = 'orange'
-# c.colors.statusbar.url.hover.fg = 'blue'
-# c.colors.statusbar.url.success.http.fg = 'black'
-# c.colors.statusbar.url.success.https.fg = 'black'
-# c.colors.statusbar.private.fg = 'black'
-# c.colors.statusbar.command.fg = 'black'
 c.colors.tabs.selected.odd.bg = '#0088cc'
 c.colors.tabs.selected.even.bg = '#0088cc'
 c.colors.tabs.even.bg = '#1c375f'
@@ -52,6 +34,7 @@ c.colors.completion.item.selected.border.top = 'black'
 
 # keybindings
 config.unbind('d')
+config.unbind('<Ctrl-q>')
 config.bind('qq', 'close')
 config.bind('q', 'tab-close')
 config.bind('<Meta-R>', 'reload')
