4c9401c414b873c7aabcd93b8ed6a52c405059a8
David Paul Graham <dpgraham4401@gmail.com>
2025-08-20 19:35:30 -0400

    fix vim problem where greater than symbol creates a delay

--- nvim/.config/nvim/lua/tree-sitter.lua
+++ nvim/.config/nvim/lua/tree-sitter.lua
@@ -1,7 +1,7 @@
 -- Use Lua to confiure tree-sitter
 -- https://github.com/nvim-treesitter/nvim-treesitter
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "javascript", "typescript", "python", "vim", "json", "html", "java", "tsx", "lua" },
+    ensure_installed = { "javascript", "typescript", "python", "vim", "json", "html", "java", "tsx", "lua", "cpp" },
     sync_install = false,
     auto_install = true,
     highlight = {
