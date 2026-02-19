00be7b5726c18ca5c1e8e959ddcf0fdae8e0b0ba
David Paul Graham <dpgraham4401@gmail.com>
2026-02-18 21:12:42 -0500

    move general PATH modifications to a module, not sure this is best move

---
+++ shell/.sh/10-path.sh
@@ -0,0 +1,10 @@
+# general PATH setup
+case ":${PATH}:" in
+    *:"$HOME/.local/bin":*)
+        ;;
+    *)
+        export PATH="$HOME/.local/bin:$PATH"
+        ;;
+esac
+
+export PATH="${PATH}:${HOME}/.local/bin/"
